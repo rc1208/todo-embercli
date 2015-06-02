6d6594854a8ca95b692e60a7e83bca1ede877072
rc1208 <chowdhuryrahul5@gmail.com>
2015-06-02 12:48:36 +0530

    added fixture adapter

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,5 +1,26 @@
 import DS from "ember-data";
-export default DS.Model.extend({
+var Todo = DS.Model.extend({
     title: DS.attr('string'),
     isCompleted : DS.attr('boolean')
 });
+Todo.reopenClass({
+  FIXTURES : [
+      {
+          id:1,
+          title:"learn ember.js",
+          isCompleted:true
+      },
+      {
+          id:2,
+          title:"learn ember.js again",
+          isCompleted:true
+      },
+      {
+          id:3,
+          title:"revise ember.js",
+          isCompleted:false
+      }
+  ]
+});
+
+export default Todo;
